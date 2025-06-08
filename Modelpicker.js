1e02c419838861cb35f3a4dbc6bac2fb1dc4e975
maximcodepro1337 <m.kirichenko@horo.school>
2025-06-08 21:52:57 +0300

    Update Modelpicker.js

--- Modelpicker.js
+++ Modelpicker.js
@@ -1,4 +1,4 @@
-import { calendarTexturesByLevel, defaultModel } from './textures.js';
+import { calendarTexturesByLevel, defaultModel, calendarTexturesByLevel1 } from './textures.js';
 
 export function getRandomModelForLevel(level) {
   const models = calendarTexturesByLevel[level];
@@ -14,3 +14,18 @@ export function getRandomModelForLevel(level) {
 
   return models[models.length - 1] || defaultModel;
 }
+
+export function getRandomModelForLevel1(level) {
+    const models = calendarTexturesByLevel1[level];
+    if (!models || models.length === 0) return defaultModel;
+  
+    const totalChance = models.reduce((sum, model) => sum + model.chance, 0);
+    let rnd = Math.random() * totalChance;
+  
+    for (const model of models) {
+      if (rnd < model.chance) return model;
+      rnd -= model.chance;
+    }
+  
+    return models[models.length - 1] || defaultModel;
+  }
